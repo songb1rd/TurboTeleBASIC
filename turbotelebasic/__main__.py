d0e3bea4b8ec460cf02aee4a6f59a9151731bb61
songb1rd <49145061+songb1rd@users.noreply.github.com>
2020-02-07 16:33:42 +0000

    Start working on lark grammar.

--- turbotelebasic/__main__.py
+++ turbotelebasic/__main__.py
@@ -1,5 +1,22 @@
-def main():
+import logging
+from io import TextIOWrapper
+from pathlib import Path
+
+import click
+from click import File
+from lark import Lark
+
+logging.basicConfig(level=logging.DEBUG)
+
+with open(Path(__file__).absolute().parent / "telebasic.lark") as inf:
+    TELEBASIC_GRAMMAR = inf.read()
+
+@click.command()
+@click.argument("input_file", type=File("r"), required=False)
+def main(input_file: TextIOWrapper):
     """TuboTeleBASIC entry point."""
+    p = Lark(TELEBASIC_GRAMMAR, start="start", parser='lalr', debug=True)
+    print(p.parse(input_file.read()))
 
 if __name__ == "__main__":
     main()
